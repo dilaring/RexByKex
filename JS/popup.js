5848bc293905996f747a215398577a285b3bfe76
dilaring <128255990+dilaring@users.noreply.github.com>
2023-03-18 22:01:02 +0200

    Add files via upload

---
+++ JS/popup.js
@@ -0,0 +1,8 @@
+function openPopup() {
+    document.getElementById("myPopup").style.display = "block";
+  }
+  
+  function closePopup() {
+    document.getElementById("myPopup").style.display = "none";
+  }
+  
